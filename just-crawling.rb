2b8b823f8e2f73d88c7d3c113706a689c51d5530
mizutaki <mizutaki4@gmail.com>
2014-11-07 22:44:21 +0900

    to chapter 2-5-2

---
+++ just-crawling.rb
@@ -0,0 +1,28 @@
+require 'anemone'
+
+#巡回クローラ
+
+#クロールの起点となるURLを指定
+urls = [
+  "http://www.amazon.co.jp/gp/bestsellers/books/",
+  "http://www.amazon.co.jp/gp/bestsellers/digital-text/2275256051/",
+  "http://www.amazon.co.jp/gp/bestsellers/dvd/"]
+
+Anemone.crawl(
+  urls, :depth_limit => 1, :skip_query_strings => true) do |anemone|
+
+  #巡回先の絞り込み
+  anemone.focus_crawl do |page|
+    page.links.keep_if { |link|
+      link.to_s.match(
+        /\/gp\/bestsellers\/books|\/gp\/bestsellers\/digital-text|\/gp\/bestsellers\/dvd/)
+    }
+  end
+
+  #取得したページに対する処理
+  anemone.on_every_page do |page|
+   puts page.url
+  end
+end
+
+  
--- webrick0.rb
+++
@@ -1,6 +0,0 @@
-require 'webrick'
-Thread.start {
-  WEBrick::HTTPServer.new(DocumentRoot:".",
-    Port:7777, BindAddress:"127.0.0.1").start
-}
-gets	
