90b2fa537e88ca3497fb106df2137dade8346c7d
mizutaki <mizutaki4@gmail.com>
2014-11-15 15:22:22 +0900

    to chapter 4-1-5

--- jis.rb
+++
@@ -1,8 +0,0 @@
-require 'nokogiri'
-require 'open-uri'
-
-html = open(
-  "http://www.amazon.co.jp/gp/bestsellers/","r:shift_JIS")
-doc =Nokogiri.HTML(html, nil, "ShiftJIS")
-
-puts doc.xpath('//title').text
--- utf.rb
+++
@@ -1,7 +0,0 @@
-require 'nokogiri'
-require 'open-uri'
-
-html = open(
-  "http://www.amazon.co.jp/gp/bestsellers/").read.encode("UTF-8", "Shift-JIS", :invalid => :replace, :undef => :replace)
-doc = Nokogiri.HTML(html)
-puts doc.xpath('//title').text
