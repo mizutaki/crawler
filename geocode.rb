e0b83395cdc6e92705594ebef826c103f02141e5
mizutaki <mizutaki4@gmail.com>
2014-11-25 21:19:40 +0900

    to chapter 5-17-2

---
+++ geocode.rb
@@ -0,0 +1,15 @@
+require 'geocoder'
+
+Geocoder.configure(:language => "ja", :units => "km")
+
+address = Geocoder.search("550-0014", :params => {:countorycodes => "ja"})
+address.each {|address|
+  address.data["address_components"].each {|value|
+    if value["short_name"] == "JP"
+      lat = address.data["geometry"]["location"]["lat"]
+      lng = address.data["geometry"]["location"]["lng"]
+      puts "経度:#{lat}"
+      puts "緯度:#{lng}"
+    end
+  }
+}
