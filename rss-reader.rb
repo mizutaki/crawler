20e8a1c984313ca4feb2bf22a4201c793e0b8653
mizutaki <mizutaki4@gmail.com>
2014-12-05 22:22:16 +0900

    Until db registration

--- rss-reader.rb
+++ rss-reader.rb
@@ -9,13 +9,30 @@ url = ''
 #pn ページ数
 #pc 表示件数 10,20,30,50,100
 xml = Nokogiri::XML(open(url).read)
-#puts xml.xpath('/rss/channel')
 item_nodes = xml.xpath('//item')
 count = 0
+arr = []
 item_nodes.each do |item|
   count += 1
   title = item.xpath('title').text
   link = item.xpath('link').text
   description = item.xpath('description').text
-  client.query("INSERT INTO residence(id, url, description) VALUES (#{count},'#{link}', '#{description}')")
+  n = description.gsub("：", "").to_s
+  o = n.scan(/\S+/)#空白文字[\t\r\n\f]以外にマッチ
+  warking_time = o[1]#バス徒歩
+  rent = o[3]#賃料管理費・共益費
+  deposit = o[5]#敷金・礼金
+  layout = o[7]#間取り
+  azimuth = o[9]#方位
+  building_year = o[11]#築年
+  real_estate_company_name = o[13]#不動産会社名
+  real_estate_company_phonenumber = o[15]#不動産会社電話
+  client.query("INSERT INTO residence(id, url, warking_time, rent, deposit, layout, azimuth, building_year, real_estate_company_name, real_estate_company_phonenumber) VALUES (#{count},'#{link}', '#{warking_time}', '#{rent}', '#{deposit}', '#{layout}','#{azimuth}','#{building_year}', '#{real_estate_company_name}', '#{real_estate_company_phonenumber}')")
 end
+
+result = client.query("SELECT * FROM residence")
+result.each do |item|
+  puts item["building_year"]
+end
+
+
