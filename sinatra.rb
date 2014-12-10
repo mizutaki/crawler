00ec8031596ce74bb9907d57cf752e6c2cbfae19
mizutaki <mizutaki4@gmail.com>
2014-12-10 21:45:22 +0900

    It was to be able to get more information

--- sinatra.rb
+++ sinatra.rb
@@ -8,15 +8,16 @@ require_relative 'db_operation'
 get '/' do
 	op = DBOperation.new
 	@residence_info = op.show
-  puts 'sinatra!'
   erb :index
 end
 
 get '/details/*' do
-	url = params[:splat][0]
-	puts params[:ar]
-	puts params[:bs]
-	puts params[:bc]
-	#TODO URLを結合して、リクエストを送る
-	JSON.generate({"foo" => "bar"})
+	url = params[:splat][0].gsub("http:/", "https://")
+	ar = params[:ar]
+	bs = params[:bs]
+	bc = params[:bc]
+	request_url = url + "?ar=#{ar}&bs=#{bs}&bc=#{bc}"
+	#詳細情報を取得する
+	html = Nokogiri::HTML(open("#{request_url}").read)
+	html.xpath("/html/body/div[1]/div[5]/div[6]/table")
 end
